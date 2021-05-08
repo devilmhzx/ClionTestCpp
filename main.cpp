25b8fb4fd12eca8c79b632769a4f61430bf10431
WeiLi <241681095@qq.com>
2021-05-08 16:50:12 +0800

    设置传入参数得方式：传引用和传指针！

--- main.cpp
+++ main.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
+#include <string>
+using namespace std;
+
+void TestA(string &str) {
+  // printf("str = %s \r\n",&str);
+  cout << "str = " << str << endl;
+  str = "你个逗逼！";
+}
+
+void TestB(string *str) {
+  cout << "str = " << *str << endl;
+  *str = "你才个逗逼！";
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+  std::cout << "Hello, World!" << std::endl;
+  string A = "我是你爸爸!";
+  TestA(A);
+
+  TestA(A);
+
+  string B = "你是我孙子！";
+
+  string *C = &B;
+  TestB(C);
+  TestB(C);
+
+  TestB(&B);
+  return 0;
 }
